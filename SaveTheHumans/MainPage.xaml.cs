bff1851163138352b14f1ce820319f286fdd4ea2
Syashin <rf777rf777@gmail.com>
2016-07-03 18:51:58 +0800

    加入AddEnemy( )與AnimateEnemy( )方法

--- SaveTheHumans/MainPage.xaml.cs
+++ SaveTheHumans/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Media.Animation;
+
 namespace SaveTheHumans
 {
     /// <summary>
@@ -20,11 +22,46 @@ namespace SaveTheHumans
     /// </summary>
     public partial class MainPage : Page
     {
+        Random rand = new Random();
+
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddEnemy();
+        }
 
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy,rand.Next((int)playArea.ActualHeight-100),rand.Next((int)playArea.ActualHeight-100),"(Canvas.Top)");
+            playArea.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard()
+            {
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(rand.Next(4,6)))
+            };
+
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation, new PropertyPath(propertyToAnimate));
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
     }
 }
